119110e430578d466f4f4371590375e9ea851a96
eileen <eileen914@snu.ac.kr>
2025-05-03 17:19:03 +0900

    seminar-commit

---
+++ post/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+# swagger 임포트
+from drf_yasg.utils import swagger_auto_schema
+
+
+class PostListView(APIView):
+  @swagger_auto_schema(
+            operation_id='게시글 목록 조회',
+            operation_description='게시글 목록을 조회합니다.',
+            responses={200: PostSerializer(many=True)}
+        )
+  ### 얘네가 class inner function 들! ###
+  def get(self, request): 
+      posts = Post.objects.all() # Post를 다 가져와라
+      serializer = PostSerializer(posts, many=True)
+      return Response(serializer.data, status=status.HTTP_200_OK)
+        
+  @swagger_auto_schema(
+          operation_id='게시글 생성',
+          operation_description='게시글을 생성합니다.',
+          request_body=PostSerializer,
+          responses={201: PostSerializer}
+      )
+  def post(self, request):
+      title = request.data.get('title')
+      content = request.data.get('content')
+      if not title or not content:
+          return Response({"detail": "[title, content] fields missing."}, status=status.HTTP_400_BAD_REQUEST)
+      post = Post.objects.create(title=title, content=content)
+      serializer = PostSerializer(post)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+# Create your views here.
+class PostDetailView(APIView):
+  @swagger_auto_schema(
+            operation_id='게시글 상세 조회',
+            operation_description='게시글 1개의 상세 정보를 조회합니다.',
+            responses={200: PostSerializer}
+        )
+  def get(self, request, post_id):
+      try:
+          post = Post.objects.get(id=post_id)
+      except:
+          return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+      serializer = PostSerializer(post)
+      return Response(serializer.data, status=status.HTTP_200_OK)
+  @swagger_auto_schema(
+            operation_id='게시글 삭제',
+            operation_description='게시글을 삭제합니다.',
+            responses={204: 'No Content', 404: 'Not Found'}
+        )  
+  def delete(self, request, post_id):
+      try:
+          post = Post.objects.get(id=post_id)
+      except:
+          return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+      post.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
