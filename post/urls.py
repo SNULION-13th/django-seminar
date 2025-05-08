83187f6acd1372da51211c4588c414f6f53ef5f1
Imggaggu <ggaggu@snu.ac.kr>
2025-05-08 17:14:23 +0900

    [Feat] create put api(Update)

--- post/urls.py
+++ post/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     # CBV url path
     path("", PostListView.as_view()), ### 추가
     path("<int:post_id>/", PostDetailView.as_view()), ### 추가
-
 ]
--- post/views.py
+++ post/views.py
@@ -80,3 +80,26 @@ class PostDetailView(APIView):
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    @swagger_auto_schema(
+        operation_id='게시글 수정',
+        operation_description='게시글을 수정합니다.',
+        request_body=PostSerializer,
+        responses={200: PostSerializer, 400:"Wrong Request", 404:"missing post"}
+      )
+    
+    def put(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+                return Response({"detail": " post missing."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        title = request.data.get('title')
+        content = request.data.get('content')
+        post.title = title
+        post.content = content
+        post.save() 
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
