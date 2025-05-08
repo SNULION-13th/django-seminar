884925d0695885a1edc553277458e6ccb42033bc
eileen <eileen914@snu.ac.kr>
2025-05-08 11:48:29 +0900

    feat put 기능 추가

--- post/views.py
+++ post/views.py
@@ -35,7 +35,6 @@ class PostListView(APIView):
       serializer = PostSerializer(post)
       return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-# Create your views here.
 class PostDetailView(APIView):
   @swagger_auto_schema(
             operation_id='게시글 상세 조회',
@@ -49,6 +48,7 @@ class PostDetailView(APIView):
           return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
       serializer = PostSerializer(post)
       return Response(serializer.data, status=status.HTTP_200_OK)
+  
   @swagger_auto_schema(
             operation_id='게시글 삭제',
             operation_description='게시글을 삭제합니다.',
@@ -61,3 +61,20 @@ class PostDetailView(APIView):
           return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
       post.delete()
       return Response(status=status.HTTP_204_NO_CONTENT)
+    
+  @swagger_auto_schema(
+            operation_id='게시글 수정',
+            operation_description='게시글을 수정합니다.',
+            request_body=PostSerializer,
+            responses={200: PostSerializer, 404: 'Not Found'}
+        )
+  def put(self, request, post_id):
+      try:
+          post = Post.objects.get(id=post_id)
+      except:
+          return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+      serializer = PostSerializer(post, data=request.data, partial=True)
+      if serializer.is_valid():
+          serializer.save()
+          return Response(serializer.data, status=status.HTTP_200_OK)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
