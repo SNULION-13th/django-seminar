5723cf96c2ccea141b7edb1c99ad7e733d2d9323
lili <suanchoi1102@snu.ac.kr>
2025-05-08 12:21:26 +0900

    7주차 과제 구현

--- post/views.py
+++ post/views.py
@@ -82,4 +82,3 @@ class PostDetailView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
