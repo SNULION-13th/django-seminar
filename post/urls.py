3054a7cab1772bd7eb0a4ae7abddc0458ecf8e67
lili <suanchoi1102@snu.ac.kr>
2025-05-10 16:50:22 +0900

    8주차 세미나

--- post/urls.py
+++ post/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-### 추가
-from .views import PostListView, PostDetailView
-###
+from .views import PostListView, PostDetailView, LikeView
 
 app_name = 'post'
 urlpatterns = [
     # CBV url path
-    path("", PostListView.as_view()), ### 추가
-    path("<int:post_id>/", PostDetailView.as_view()), ### 추가
+    path("", PostListView.as_view()),
+    path("<int:post_id>/", PostDetailView.as_view()),
+    path("<int:post_id>/like/", LikeView.as_view()),
 ]
