a3bad1adfb67ee1839527c0fa466382c65816fac
ajy1216 <ajy1216@snu.ac.kr>
2025-05-10 16:50:30 +0900

    Week8-seminar

--- seminar/settings.py
+++ seminar/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'post',  #추가, 늘여서 'post.apps.PostConfig'라고 적어도 된다는 비밀 아닌 비밀
+    'account',
+    'tag',
     'drf_yasg',
 ]
 
