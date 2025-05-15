5599679dfd70716d8a79513e0628656d6a08c970
eileen <eileen914@snu.ac.kr>
2025-05-15 21:58:28 +0900

    feat 세부기능 4가지 구현

--- comment/serializers.py
+++ comment/serializers.py
@@ -5,3 +5,6 @@ class CommentSerializer(ModelSerializer):
     class Meta:
         model = Comment
         fields = "__all__"
+        extra_kwargs = {
+            "author": {"allow_null": False},  ## swagger에서 author가 x-nullable: true로 나와서
+        }
